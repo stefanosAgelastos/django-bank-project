dd318fe0216d03623ffeb8c61ad3087a4c69f05e
Henrik Strøm <henrik@stroem.net>
2021-09-27 10:50:18 +0200

    Register models to amdin.

--- bank/admin.py
+++ bank/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Rank, Customer, Account, Ledger
 
-# Register your models here.
+admin.site.register(Rank)
+admin.site.register(Customer)
+admin.site.register(Account)
+admin.site.register(Ledger)
+
