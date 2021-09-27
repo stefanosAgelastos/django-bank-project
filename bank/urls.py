099686c97751da7c2fff99862df78bdabd320551
Henrik Strøm <henrik@stroem.net>
2021-09-27 12:35:44 +0200

    Separate routing of customer and teller index.

--- bank/urls.py
+++ bank/urls.py
@@ -6,4 +6,6 @@ app_name = "bank"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index_customer/', views.index_customer, name='index_customer'),
+    path('index_teller/', views.index_teller, name='index_teller'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def index(request):
+    if request.user.is_staff:
+        return HttpResponseRedirect(reverse('bank:index_teller'))
+    else:
+        return HttpResponseRedirect(reverse('bank:index_customer'))
+
+
+@login_required
+def index_customer(request):
+    assert not request.user.is_staff, 'Staff user routing customer view.'
+    context = {}
+    return render(request, 'bank/index.html', context)
+
+
+@login_required
+def index_teller(request):
+    assert request.user.is_staff, 'Customer user routing staff view.'
     context = {}
     return render(request, 'bank/index.html', context)
