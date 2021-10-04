9da5772eedc06a06fb38ffd9b30516e9916d2ac3
Henrik Strøm <henrik@stroem.net>
2021-10-04 12:01:55 +0200

    Simplification and change naming scheme.

--- bank/urls.py
+++ bank/urls.py
@@ -6,9 +6,6 @@ app_name = "bank"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index_customer/', views.index_customer, name='index_customer'),
-    path('index_teller/', views.index_teller, name='index_teller'),
-    path('profile_customer/', views.profile_customer, name='profile_customer'),
-    path('profile_customer_update_user/', views.profile_customer_update_user, name='profile_customer_update_user'),
-    path('profile_customer_update_customer/', views.profile_customer_update_customer, name='profile_customer_update_customer'),
+    path('dashboard/', views.index_customer, name='dashboard'),
+    path('staff_dashboard/', views.index_teller, name='staff_dashboard'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -8,56 +8,20 @@ from .models import Customer
 @login_required
 def index(request):
     if request.user.is_staff:
-        return HttpResponseRedirect(reverse('bank:index_teller'))
+        return HttpResponseRedirect(reverse('bank:staff_dashboard'))
     else:
-        try:
-            request.user.customer
-            return HttpResponseRedirect(reverse('bank:index_customer'))
-        except: # Customer.RelatedObjectDoesNotExist:
-            return HttpResponseRedirect(reverse('bank:profile_customer'))
+        return HttpResponseRedirect(reverse('bank:dashboard'))
 
 
 @login_required
-def index_customer(request):
+def dashboard(request):
     assert not request.user.is_staff, 'Staff user routing customer view.'
     context = {}
-    return render(request, 'bank/index_customer.html', context)
+    return render(request, 'bank/dashboard.html', context)
 
 
 @login_required
-def profile_customer(request):
-    assert not request.user.is_staff, 'Staff user routing customer view.'
-    user_form = UserForm(instance=request.user)
-    try:
-        customer_profile_form = CustomerProfileForm(instance=request.user.customer)
-    except:
-        customer_profile_form = CustomerProfileForm()
-
-    context = {
-        'user_form': user_form,
-        'customer_profile_form': customer_profile_form,
-    }
-
-    return render(request, 'bank/profile.html', context)
-
-
-@login_required
-def profile_customer_update_user(request):
-    pass
-
-
-@login_required
-def profile_customer_update_customer(request):
-    form = CustomerProfileForm(request.POST)
-    if form.is_valid():
-        form['user'] = request.user
-        form.save()
-    context = { 'user_profile_form': form }
-    return render(request, 'bank/profile_customer_update_customer_partial.html', context)
-
-
-@login_required
-def index_teller(request):
+def staff_index(request):
     assert request.user.is_staff, 'Customer user routing staff view.'
     context = {}
-    return render(request, 'bank/index_teller.html', context)
+    return render(request, 'bank/staff_index.html', context)
