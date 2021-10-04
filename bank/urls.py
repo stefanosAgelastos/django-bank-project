212281b6b87a0de5e0d33f7aaf0f697d5f7130f1
Henrik Strøm <henrik@stroem.net>
2021-10-04 16:36:12 +0200

    Details view for customer.

--- bank/urls.py
+++ bank/urls.py
@@ -6,6 +6,9 @@ app_name = "bank"
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('details/<int:pk>/', views.details, name='details'),
+
     path('staff_dashboard/', views.staff_dashboard, name='staff_dashboard'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Customer, Account
+from .models import Account
 
 
 @login_required
@@ -12,12 +12,35 @@ def index(request):
         return HttpResponseRedirect(reverse('bank:dashboard'))
 
 
+# Customer views
+
 @login_required
 def dashboard(request):
     assert not request.user.is_staff, 'Staff user routing customer view.'
-    context = {}
+
+    accounts = request.user.customer.accounts
+
+    context = {
+        'accounts': accounts,
+    }
     return render(request, 'bank/dashboard.html', context)
 
+
+@login_required
+def details(request, pk):
+    assert not request.user.is_staff, 'Staff user routing customer view.'
+
+    account = get_object_or_404(Account, user=request.user, pk=pk)
+
+    context = {
+        'account': pk,
+        'movements': account.movements,
+        'balance': account.balance
+    }
+    return render(request, 'bank/details.html', context)
+
+
+# Staff views
 
 @login_required
 def staff_dashboard(request):
