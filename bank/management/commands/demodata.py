dd266c9af739401adbc2fc0fd4f460a7f691bc94
Henrik Strøm <henrik@stroem.net>
2021-10-05 14:39:10 +0200

    More demo data.

--- bank/management/commands/demodata.py
+++ bank/management/commands/demodata.py
@@ -1,7 +1,7 @@
 import secrets
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-from bank.models import Account, Ledger
+from bank.models import Account, Ledger, Customer
 
 
 class Command(BaseCommand):
@@ -21,3 +21,22 @@ class Command(BaseCommand):
             'Operational Credit',
             is_loan=True
         )
+
+        dummy_user = User.objects.create_user('dummy', email='dummy@dummy.com', password='mirror12')
+        dummy_user.first_name = 'Dummy'
+        dummy_user.last_name  = 'Dimwit'
+        dummy_user.save()
+        dummy_customer = Customer(user=dummy_user, personal_id='555666', phone='555666')
+        dummy_customer.save()
+        dummy_account = Account.objects.create(user=dummy_user, name='Checking account')
+        dummy_account.save()
+
+        Ledger.transfer(
+            1_000,
+            ops_account,
+            'Payout to dummy',
+            dummy_account,
+            'Payout from bank'
+        )
+
+
